f56dad5fb2da5e239805218aae91ea27492d646f
Filipe Nicoli <fibean@hotmail.com>
2018-10-31 00:20:16 -0300

    v0.0.1

---
+++ toggle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+TINT2FOLDER="/home/filipe/.config/tint2"
+
+# Configura display para que possa ser executado fora do terminal
+export DISPLAY=:0
+
+if [ -f mochaccino.enabled ]; then
+	rm mochaccino.enabled;
+	notify-send -i $SCRIPTPATH/icone_vazio.png "mochaccino off" "A tela desligará em `xset q | grep Standby | awk '{print $2}'` segundos de inatividade";
+	xset +dpms
+	killall tint2
+	tint2 -c $TINT2FOLDER/tint2rc &
+else
+	touch mochaccino.enabled;
+	notify-send -i $SCRIPTPATH/icone_cheio.png "mochaccino on" "A tela permanecerá ligada`dirname`";
+	xset -dpms
+	killall tint2
+	tint2 -c $TINT2FOLDER/tint2rc-red &
+fi
